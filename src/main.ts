f957f597f509f14980f2073279a6bf47dbae8860
mujeebcodes <mailmujeeb@icloud.com>
2024-03-13 21:50:07 +0000

    configured cors to accept all request

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*',
-    // credentials: true,
+    origin: 'http://localhost:5173',
+    credentials: true,
   });
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));

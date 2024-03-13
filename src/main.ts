c4412c5001e9b2714ca2af52d32710b79ac940f0
mujeebcodes <mailmujeeb@icloud.com>
2024-03-13 21:37:36 +0000

    configured cors to accept all request

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,10 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // app.enableCors({
-  //   origin: 'http://localhost:5173',
-  //   credentials: true,
-  // });
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+  });
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.use(cookieParser());

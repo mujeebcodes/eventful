76ce95857b04211e7d9c3e1358b9354115dca281
mujeebcodes <mailmujeeb@icloud.com>
2024-03-15 12:12:23 +0000

    switching to mongodb

--- prisma/migrations/20240314184431_changed_when_to_remind_datatype/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Changed the type of `whenToRemind` on the `Enrollment` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
-
-*/
--- AlterTable
-ALTER TABLE "Enrollment" DROP COLUMN "whenToRemind",
-ADD COLUMN     "whenToRemind" TIMESTAMP(3) NOT NULL;

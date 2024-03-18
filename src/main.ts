1695a8a035cbed8f46e08a86b446161d5649fd6b
mujeebcodes <mailmujeeb@icloud.com>
2024-03-18 23:30:09 +0000

    enabling cors

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,10 @@ import { CreateOrganizerDto } from './organizer/dto/create-organizer.dto';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // app.enableCors({
-  //   origin: 'http://localhost:5173',
-  //   credentials: true,
-  // });
+  app.enableCors({
+    origin: 'https://eventful-udoh.onrender.com',
+    credentials: true,
+  });
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.use(cookieParser());
